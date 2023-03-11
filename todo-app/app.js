fb7286b5d86d085aa520cca91acd1fef6a0af34c
harish96880 <sriharishr105@gmail.com>
2023-03-11 11:28:37 +0530

    Adding Todo App

---
+++ todo-app/app.js
@@ -0,0 +1,43 @@
+const { request, response } = require("express");
+const express = require("express");
+const app = express();
+const { Todo } = require("./models");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.get("/todos", (request, response) => {
+  console.log("Todo list");
+});
+
+app.post("/todos", async (request, response) => {
+  console.log("Creating a todo", request.body);
+  try {
+    const todo = await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+      completed: false,
+    });
+    return response.json(todo);
+  } catch (error) {
+    console.log(erro);
+    return response.status(422).json(error);
+  }
+});
+
+app.put("/todos/:id/markAsCompleted", async (request, response) => {
+  console.log("We have to a todo with ID:", request.params.id);
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const updatedTodo = await todo.markAsCompleted();
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.delete("todos/:id", (request, response) => {
+  console.log("Delet a todo by ID:", request.params.id);
+});
+
+module.exports = app;
